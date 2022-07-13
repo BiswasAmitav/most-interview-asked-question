509c103256449fde3f6266bcb317ef8e853a7b7b
BiswasAmitav <biswasamit2648@gmail.com>
2022-07-13 22:52:54 +0530

    array initialized and and print

---
+++ technical interview question/src/arrayInJavaQs/InitializedAndPrint.java
@@ -0,0 +1,13 @@
+package arrayInJavaQs;
+
+public class InitializedAndPrint {
+    public static void main(String[] args) {
+        int a[] = {20,30,40,50,60};
+
+        //print array..
+        for(int i=0;i<a.length;i++){
+            System.out.print(a[i]+"\t");
+        }
+        System.out.println(". ");
+    }
+}
